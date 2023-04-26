036e1ad4507af4b2bde33b131acc8ee05b5f8306
Andiiiiie <andiejessica2020@gmail.com>
2023-04-27 02:42:56 +0300

    Statut entreprise

---
+++ comptable/views/status_entreprise_view.py
@@ -0,0 +1,118 @@
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.core.files.storage import FileSystemStorage
+from django.db import transaction
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+
+from comptable.models import StatusEntreprise
+
+nom_simple = 'statut'
+nom_avec_article = 'un statut'
+nom_avec_article_def = 'le statut'
+
+template_form = 'status_entreprise/form.html'
+list_template = 'status_entreprise/list.html'
+
+
+@login_required
+def create(request):
+    context = {
+        'title': 'Ajouter ' + nom_avec_article,
+        'action': 'Ajouter',
+        'icon': 'fas fa-save',
+        'color': 'success',
+    }
+    if request.method == 'POST':
+        try:
+            # DEBUT TODO
+            StatusEntreprise.create(request.POST['nom'], request.POST['sigle'])
+            # FIN
+            context['success'] = [nom_avec_article_def + ' a été créé avec succès']
+        except ValidationError as e:
+            context['nom'] = request.POST['nom']
+            context['sigle'] = request.POST['sigle']
+            context['errors'] = e.messages
+    return render(request, template_form, context)
+
+
+@login_required
+def update(request, id_object):
+    # DEBUT TODO
+    object_i = get_object_or_404(StatusEntreprise, pk=id_object)
+    context = {
+        'sigle': object_i.sigle,
+        'nom': object_i.nom,
+
+    }
+    # FIN TODO
+    if request.GET.get('remove') is not None:
+        context.update({
+            'title': 'Supprimer ' + nom_avec_article,
+            'action': 'Confirmer la suppression',
+            'form_action': "action='" + reverse('remove_' + nom_simple, args=[id_object]) + "'",
+            'remove': True,
+            'icon': 'fas fa-trash-alt',
+            'color': 'danger',
+        })
+    else:
+        context.update({
+            'title': 'Modifier ' + nom_avec_article,
+            'action': 'Modifier',
+            'icon': 'fas fa-pencil-alt',
+            'color': 'warning',
+        })
+    if request.method == 'POST':
+        try:
+            # DEBUT TODO
+            context.update({
+                'sigle': request.POST['sigle'],
+                'nom': request.POST['nom'],
+
+            })
+            object_i.update(request.POST['nom'], request.POST['sigle'])
+            # FIN
+            context['success'] = [nom_avec_article_def + ' a été modifié avec succès']
+        except ValidationError as e:
+            context['errors'] = e.messages
+    return render(request, template_form, context)
+
+
+@login_required
+def remove(request, id_object):
+    if request.method != 'POST':
+        return redirect('list_' + nom_simple)
+    context = {}
+    # DEBUT TODO
+    object_i = get_object_or_404(StatusEntreprise, pk=id_object)
+    # FIN
+    object_i.remove()
+    context['success'] = [nom_avec_article_def + ' a été supprimé avec succès']
+    return redirect('list_' + 'status_entreprise')
+
+
+@login_required
+def read(request):
+    # DEBUT TODO
+    context = {nom_simple + 's': StatusEntreprise.objects.all()}
+    # FIN
+    return render(request, list_template, context)
+
+@login_required
+@transaction.atomic
+def import_from_csv(request):
+    context = {nom_simple + 's': StatusEntreprise.objects.all()}
+    if request.method == 'POST':
+        try:
+            uploaded_file = request.FILES['file']
+            fs = FileSystemStorage(location='csv_files/')
+            filename = fs.save(uploaded_file.name, uploaded_file)
+            file_path = fs.path(filename)
+            StatusEntreprise.import_from_csv(file_path,request.POST.get("header") is not None)
+            context.update({'success': ['Les statuts d\'entreprise ont été importés avec succès']})
+        except ValidationError as e:
+            context.update({'errors': e.messages})
+        except Exception:
+            context.update({'errors': ["Le traitement du fichier a échoué! <br> Vérifier que le fichier est un .csv et "
+                                  "l'ordre des colonnes"]})
+    return render(request, list_template,context)
